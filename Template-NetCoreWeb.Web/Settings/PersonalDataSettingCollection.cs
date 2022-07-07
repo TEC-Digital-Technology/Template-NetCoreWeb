2903d285df3f634d4be7403e17eddfa47dcfeadd
Antony <Antony@tecyt.com>
2022-07-07 16:57:57 +0800

    加入 PersonalDataSettingCollection 的 JSON 支援

--- Template-NetCoreWeb.Web/Settings/PersonalDataSettingCollection.cs
+++ Template-NetCoreWeb.Web/Settings/PersonalDataSettingCollection.cs
@@ -1,4 +1,5 @@
-﻿using TEC.Core.Settings.Collections;
+﻿using Newtonsoft.Json;
+using TEC.Core.Settings.Collections;
 using Template_NetCoreWeb.Utils.Enums.Settings;
 
 namespace Template_NetCoreWeb.WebMvc.Settings
@@ -19,6 +20,40 @@ namespace Template_NetCoreWeb.WebMvc.Settings
         }
 
         /// <summary>
+        /// 將指定的物件序列化為 Json ，並儲存至 Session 中
+        /// </summary>
+        /// <typeparam name="T">要序列化的型別</typeparam>
+        /// <param name="personalDataSetting">索引鍵</param>
+        /// <param name="value">要序列化的值</param>
+        /// <param name="converters">參與序列化的值轉換器</param>
+        public void SetValueAsJson<T>(PersonalDataSettingEnum personalDataSetting, T value, params JsonConverter[] converters)
+        {
+            JsonSerializerSettings jsonSerializerSettings = new();
+            jsonSerializerSettings.TypeNameHandling = TypeNameHandling.All;
+            converters.ToList().ForEach(t => jsonSerializerSettings.Converters.Add(t));
+            this[personalDataSetting] = JsonConvert.SerializeObject(value, jsonSerializerSettings);
+        }
+
+        /// <summary>
+        /// 從 Session 中取得指定索引鍵的字串，透過 Json 反序列化為指定型別的物件
+        /// </summary>
+        /// <typeparam name="T">要反序列化的型別</typeparam>
+        /// <param name="personalDataSetting">索引鍵</param>
+        /// <param name="converters">參與反序列化的值轉換器</param>
+        public T? GetValueFromJson<T>(PersonalDataSettingEnum personalDataSetting, params JsonConverter[] converters)
+        {
+            JsonSerializerSettings jsonSerializerSettings = new();
+            jsonSerializerSettings.TypeNameHandling = TypeNameHandling.All;
+            converters.ToList().ForEach(t => jsonSerializerSettings.Converters.Add(t));
+            string? value = this[personalDataSetting];
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return default;
+            }
+            return JsonConvert.DeserializeObject<T>(value, jsonSerializerSettings)!;
+        }
+
+        /// <summary>
         /// 取得設定檔集合的預設值
         /// </summary>
         /// <param name="key">設定檔列舉</param>
@@ -27,6 +62,7 @@ namespace Template_NetCoreWeb.WebMvc.Settings
         {
             return this.Session.GetString(this.GetSessionKey(key));
         }
+
         /// <summary>
         /// 
         /// </summary>
